29e0f4851d42afe47186a632e06e5b05fd3f8d0a
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-21 19:06:03 +0100

    Rectangle class

--- src/main/java/com/kenny/wenny/classes/Main.java
+++ src/main/java/com/kenny/wenny/classes/Main.java
@@ -1,14 +1,11 @@
 package com.kenny.wenny.classes;
 
 import com.kenny.wenny.classes.model.academic.Student;
+import com.kenny.wenny.classes.model.geometry.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        var studentA = new Student("Ekta");
-        var studentB = new Student("Nade");
-        var studentC = new Student("Kenny");
-        System.out.println(studentA.getStudentOf());
-        System.out.println(studentB.getName());
-        System.out.println(studentC.getName());
+        var rectangle = new Rectangle(3, 4);
+        System.out.println("Area of the rectangle is " + rectangle.getArea());
     }
 }
---
+++ src/main/java/com/kenny/wenny/classes/model/geometry/Rectangle.java
@@ -0,0 +1,23 @@
+package com.kenny.wenny.classes.model.geometry;
+
+public class Rectangle {
+    private final double width;
+    private final double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
