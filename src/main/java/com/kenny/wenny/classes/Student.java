0bdb5b6611b0736b6bed3c67aee04c73fdb47441
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-21 18:41:03 +0100

    Accessors

--- src/main/java/com/kenny/wenny/classes/Student.java
+++ src/main/java/com/kenny/wenny/classes/Student.java
@@ -1,4 +1,13 @@
 package com.kenny.wenny.classes;
 
 public class Student {
+    private String studentOf = "CFG";
+
+    public String getStudentOf() {
+        return studentOf;
+    }
+
+    public void setStudentOf(String studentOf) {
+        this.studentOf = studentOf;
+    }
 }
